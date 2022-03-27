0b10e7a1f20e22af8e6c7c31d61396b92d8713c6
Alexis <amart78@outlook.com>
2022-03-26 20:58:16 -0500

    commit tree 34341bfa828c0e198722029340be28e3fcf55a84

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,24 @@
-import React from "react";
+// The button should say "OFF" when it is first displayed
+// When the button is clicked, it should say "ON" & the background color should be red
+// When the button is clicked again, it should say "OFF" & the background color should be white again
+
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick(){
+    setIsOn( (isOn) => !isOn);
+  }
+  
+  const color = isOn ? "red" : "white";
+  
+  return(
+    <button onClick = {handleClick} style = { {background:color} } >
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
